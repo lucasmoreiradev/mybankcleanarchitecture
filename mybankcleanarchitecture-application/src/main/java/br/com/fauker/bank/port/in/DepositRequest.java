57d7d770d967a4b1e515eea0d03ac9fb962185b4
Lucas Moreira <eskerlucas1@gmail.com>
2020-06-28 15:13:55 -0300

    Fix project structure Add javax validation Add Junit 5

--- mybankcleanarchitecture-application/src/main/java/br/com/fauker/bank/port/in/DepositRequest.java
+++ mybankcleanarchitecture-application/src/main/java/br/com/fauker/bank/port/in/DepositRequest.java
@@ -1,22 +1,25 @@
 package br.com.fauker.bank.port.in;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.validation.constraints.NotNull;
 
 import br.com.fauker.bank.domain.Money;
 
 public class DepositRequest {
 
+	@NotNull
 	private final Long sourceAccountId;
 	
+	@NotNull
 	private final Long targetAccountId;
 	
+	@NotNull
 	private final Money money;
 	
 	public DepositRequest(Long sourceAccountId, Long targetAccountId, Money money) {
-		assertNotNull("Source Account id is required", sourceAccountId);
-		assertNotNull("Target Account id is required", targetAccountId);
-		assertTrue(money.isPositive(), "The money should be greather than zero!");
+		if (!money.isPositive()) {
+			throw new IllegalArgumentException("Money should be positive");
+		}
 		this.sourceAccountId = sourceAccountId;
 		this.targetAccountId = targetAccountId;
 		this.money = money;
